bbc31ceadaa3ce65fb8ac71455f6f95d30538918
akku1409 <atharvakaturde9@gmail.com>
2025-09-16 19:24:47 +0530

    Add files via upload

---
+++ pythontask1.py
@@ -0,0 +1,17 @@
+# Program to perform basic mathematical operations on two numbers
+
+# Taking two numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
